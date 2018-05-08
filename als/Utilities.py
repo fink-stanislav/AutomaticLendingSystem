d942b86c5f7f8278c2c993db54e26901b8f4a375
Stanislau Fink <ravenhollm@gmail.com>
2018-05-08 18:06:12 +0300

    Removing unused module
    Module Utilities.py is not used anymore thus it is deleted.
    
    See: #4

--- als/Utilities.py
+++
@@ -1,9 +0,0 @@
-
-def list_from_file(filename):
-    try:
-        with open(filename) as f:
-            lines = f.read().splitlines()
-            return lines
-    except Exception as e:
-        print "Error: {}".format(e)
-        return []
